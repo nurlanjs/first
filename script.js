3ef4c474fa2a2c37962475975b831fea54a0dadd
nurlanjs <nurlan1992kaz@gmail.com>
2024-08-11 16:26:19 +0500

    second script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let numberOfFilms = +prompt("Сколько фильмов вы уже смотрели","");
 
+
 const perosnalMovieDB = {
     count:numberOfFilms,
     movies: {},
@@ -8,9 +9,28 @@ const perosnalMovieDB = {
     privat: false 
 };
 
-let lastFilm = prompt("Один из последних просмотренных фильмов","");
-let rating = prompt("На сколько оцените его?");
+if(perosnalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (perosnalMovieDB.count >= 10 && perosnalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else  if (perosnalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-perosnalMovieDB.movies[lastFilm] = rating;
+let lastFilm,rating;
+for(let i = 0; i < 2; i++){
+    lastFilm = prompt("Один из последних просмотренных фильмов","");
+    rating = prompt("На сколько оцените его?");
+    if(lastFilm != null && rating != null && lastFilm != "" && rating != "" && lastFilm.length < 50){
+        perosnalMovieDB.movies[lastFilm] = rating;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+    
+}
 
 console.log(perosnalMovieDB);
