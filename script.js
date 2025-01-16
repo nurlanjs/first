91a8c6b6baa64d9f4b5811006d9761034999d951
nurlanjs <nurlan1992kaz@gmail.com>
2025-01-16 21:29:21 +0500

    Add for

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const personalMovieDB = {
     privat: false
 }
 
-const a = prompt("Один из просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let a, b;
 
+for (i = 0; i < 2; i++) {
+    a = prompt("Один из просмотренных фильмов?", "");
+    b = prompt("На сколько оцените его?", "");
+    console.log(a);
+    if (!a || !a.length || a.length > 50) {
+        console.log(1);
+        i--;
+    } else {
+        personalMovieDB.movies[a] = b;
+    }
+}
+console.log(personalMovieDB.movies);
 console.log(personalMovieDB);
